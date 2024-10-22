fc2f6ac4802d9c26d23bab32f3a0b2b846d9aae4
Mac <mac@Macs-MacBook-Pro.local>
2024-10-22 14:04:39 +0300

    JSON character

--- http/main.go
+++ http/main.go
@@ -1,32 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-// Handler function for the root endpoint "/"
-// http.ResponseWriter writes a response to the client when they visit the page
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Welcome to your first go HTTP server!")
+// Define a struct to hold the incoming data
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
 
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "This is the About Page!")
-}
+// Function to handle requests for the root endpoint "/"
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		var user User
 
-func contactHomelander(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "This is the Contact Page")
-}
+		//Decode the JSON body into the user struct
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+			http.Error(w, "Invalid request body", http.StatusBadRequest)
+			return
+		}
 
+		//Respond with a message including the user's name and email
+		responseMessage := fmt.Sprintf("Received user: Name: %s, Email: %s", user.Name, user.Email)
+		fmt.Fprintf(w, responseMessage)
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+}
 func main() {
-	// Map the "/" URL to the homeHandler function
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/about", aboutHandler)
-	http.HandleFunc("/contact", contactHomelander)
+
+	// Map the "/" URL to the handleRequest function
+	http.HandleFunc("/", handleRequest)
 
 	// Start the server on port 8080
-	fmt.Println("Server is running on port 8080...")
+	fmt.Println("Server is running on port 8080....")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
