b0318fea64db0a1befb8e72b6e127b3431c35d59
Mac <mac@Macs-MacBook-Pro.local>
2024-10-22 13:13:16 +0300

    Multiple endpoints

--- http/main.go
+++ http/main.go
@@ -7,13 +7,24 @@ import (
 )
 
 // Handler function for the root endpoint "/"
+// http.ResponseWriter writes a response to the client when they visit the page
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Welcome to your first go HTTP server!")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "This is the About Page!")
+}
+
+func contactHomelander(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "This is the Contact Page")
 }
 
 func main() {
 	// Map the "/" URL to the homeHandler function
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/contact", contactHomelander)
 
 	// Start the server on port 8080
 	fmt.Println("Server is running on port 8080...")
